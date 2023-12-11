afa5fea6ace1b50121dcb639e0b96ee46c403f04
itsRohit47 <rohitbajaj13@yahoo.com>
2023-12-12 02:15:47 +1100

    updated landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,64 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center p-16 min-h-screen">
-      <div className="before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-orange-200 after:via-yellow-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-orange-700 before:dark:opacity-10 after:dark:from-orange-900 after:dark:via-[#cfcfcf] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        {" "}
-        <p className="font-Metallica text-4xl">Rohit bajaJ</p>
-      </div>
-      <span className="absolute top-20 left-0 p-16">
-        <div className="border px-4 py-2 text-sm rounded-full bg-gray-100 text-gray-500">
-          <span className="font-bold">Note:</span> This project is a work in
-          progress.
+    <main className="min-h-screen bg-zinc-50 dark:bg-slate-950">
+      <header className="p-10">
+        <p className="font-Metallica text-4xl text-center">Rohit bajaJ</p>
+      </header>
+      <section className="h-max p-2 font-normal text-gray-800 dark:text-zinc-100 text-center italic">
+        <span className="relative">
+          <span className="z-10">
+            The mind is everything. What you think you become - Buddha
+          </span>
+        </span>
+      </section>
+
+      <section className="relative h-max p-10 flex content-center justify-center">
+        <div className="flex flex-col gap-3">
+          <div className="flex content-center justify-center">
+            <Image
+              className="rounded-full shadow-lg dark:shadow-slate-50"
+              src="/profile.jpg"
+              width={170}
+              height={150}
+              alt="Rohit Bajaj"
+            ></Image>
+          </div>
+          <h1 className="h-max font-bold text-2xl text-gray-900 dark:text-zinc-100">
+            Heyyy 👋 its me, <br></br>{" "}
+            <span className="font-extrabold text-6xl tracking-widest">
+              ROHIT
+            </span>
+          </h1>
         </div>
-      </span>
+        <div className="absolute top-1/2 font-Metallica text-8xl text-gray-200 pt-24 px-10 dark:text-gray-50">
+          <h1>
+            it <br></br> graduate
+          </h1>
+        </div>
+      </section>
+      <div className="flex justify-center content-center">
+        <div className="w-max mt-20 text-center text-2xl border-2 rounded-lg mx-2 py-2 px-5 cursor-pointer bg-gray-200 text-gray-600 tracking-widest font-bold">
+          Download My Resume
+        </div>
+      </div>
+
+      <section className="mt-5 p-4 text-2xl tracking-widest text-slate-950 dark:text-zinc-100 font-bold hover:underline flex content-center justify-center">
+        {" "}
+        <ul className="flex flex-col gap-5">
+          <li>
+            <Link href="/">WHOAMI</Link>
+          </li>
+          <li>
+            <Link href="/">GET IN TOUCH</Link>
+          </li>
+          <li>
+            <Link href="/">PROJECTS</Link>
+          </li>
+        </ul>
+      </section>
     </main>
   );
 }
