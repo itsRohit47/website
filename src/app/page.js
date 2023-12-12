c67cdb5bb0c6b9ed37a086094bf57d6bdc57c20c
itsRohit47 <rohitbajaj13@yahoo.com>
2023-12-12 21:06:51 +1100

    changes to layout

--- src/app/page.js
+++ src/app/page.js
@@ -5,13 +5,18 @@ export default function Home() {
     <main className="min-h-screen overflow-x-hidden">
       <header className="p-10">
         <p className="font-Metallica text-4xl text-center">Rohit . bajaJ</p>
-        <div className="flex gap-4 mt-6 justify-center text-gray-600 font-medium">
+        <div className="text-center font-Metallica text-2xl text-gray-800  dark:text-gray-50">
+          <h1>
+            it  graduate
+          </h1>
+        </div>
+        <div className="flex gap-4 mt-6 justify-center text-gray-600 dark:text-zinc-100 font-medium">
           <Link href="/">Projects</Link>
           <h1>|</h1>
           <Link href="/">Whoami</Link>
         </div>
       </header>
-      <section className="h-max p-2 font-normal text-gray-800 dark:text-zinc-100 text-center italic">
+      <section className="h-max px-10 font-normal text-gray-800 dark:text-zinc-100 text-center italic">
         <span className="relative">
           <span>
             The mind is everything. What you think you become - Buddha
@@ -19,23 +24,23 @@ export default function Home() {
         </span>
       </section>
 
-      <section className="relative h-max p-10 flex content-center justify-center">
+      <section className="p-2 flex content-center justify-center">
         <div className="flex flex-col gap-10">
           <div className="flex content-center justify-center">
             <Image
-              className="rounded-full shadow-lg dark:shadow-slate-50"
+              className="rounded-full border mt-2"
               src="/profile.jpg"
-              width={170}
-              height={150}
+              width={100}
+              height={100}
               alt="Rohit Bajaj"
             ></Image>
           </div>
           <h1 className="h-max font-bold text-2xl text-gray-900 dark:text-zinc-100">
             Heyyy 👋 its me, <br></br>{" "}
-            <span className="font-extrabold text-6xl tracking-widest">
+            <span className="font-extrabold text-5xl tracking-widest">
               ROHIT
             </span>
-            <div className="mt-4 text-center cursor-pointer bg-gray-900 border-2 text-zinc-100 font-semibold w-max px-4 py-2 rounded-lg flex gap-2 content-center justify-center">
+            <div className="mt-2 text-lg text-center cursor-pointer bg-gray-900 border-2 shadow-xl text-zinc-100 font-semibold w-max px-4 py-2 rounded-lg flex gap-2 content-center justify-center">
               Resume{" "}
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -52,12 +57,6 @@ export default function Home() {
                 />
               </svg>
             </div>
-          </h1>
-        </div>
-
-        <div className="absolute top-1/2 font-Metallica text-8xl text-gray-200 pt-24 px-10 dark:text-gray-50 -z-10 ">
-          <h1>
-            it <br></br> graduate
           </h1>
         </div>
       </section>
