d29804de9bd24aa96e1487c380afb8df3702de26
Rohit Bajaj <108188800+itsRohit47@users.noreply.github.com>
2024-06-07 11:17:50 +0000

    Added Smooth Animations

--- src/app/page.js
+++ src/app/page.js
@@ -6,28 +6,37 @@ import Love from "../components/Love";
 import { useState, useEffect } from "react";
 
 const Welcome = [<HomePage key={1} />, <Education key={2} />];
-
 const projectComponents = [<Work key={1} />, <Love key={2} />];
 
 export default function Home() {
   const [currentComponentIndex, setCurrentComponentIndex] = useState(0);
   const [currentComponentArray, setCurrentComponentArray] = useState(Welcome);
+  const [animationClass, setAnimationClass] = useState("fade-in");
 
   function handleKeyDown(event) {
-    if (event.key === "ArrowLeft") {
-      setCurrentComponentArray(Welcome);
-      setCurrentComponentIndex(0);
-    } else if (event.key === "ArrowRight") {
-      setCurrentComponentArray(projectComponents);
-      setCurrentComponentIndex(0);
-    } else if (event.key === "ArrowDown") {
-      setCurrentComponentIndex((prevIndex) =>
-        prevIndex === currentComponentArray.length - 1 ? 0 : prevIndex + 1
-      );
-    } else if (event.key === "ArrowUp") {
-      setCurrentComponentIndex((prevIndex) =>
-        prevIndex === 0 ? currentComponentArray.length - 1 : prevIndex - 1
-      );
+    if (
+      ["ArrowLeft", "ArrowRight", "ArrowDown", "ArrowUp"].includes(event.key)
+    ) {
+      setAnimationClass("fade-out");
+
+      setTimeout(() => {
+        if (event.key === "ArrowLeft") {
+          setCurrentComponentArray(Welcome);
+          setCurrentComponentIndex(0);
+        } else if (event.key === "ArrowRight") {
+          setCurrentComponentArray(projectComponents);
+          setCurrentComponentIndex(0);
+        } else if (event.key === "ArrowDown") {
+          setCurrentComponentIndex((prevIndex) =>
+            prevIndex === currentComponentArray.length - 1 ? 0 : prevIndex + 1
+          );
+        } else if (event.key === "ArrowUp") {
+          setCurrentComponentIndex((prevIndex) =>
+            prevIndex === 0 ? currentComponentArray.length - 1 : prevIndex - 1
+          );
+        }
+        setAnimationClass("fade-in");
+      }, 500); // Match the transition duration in the CSS
     }
   }
 
@@ -36,5 +45,7 @@ export default function Home() {
     return () => document.removeEventListener("keydown", handleKeyDown);
   }, []);
 
-  return <main>{currentComponentArray[currentComponentIndex]}</main>;
+  const CurrentComponent = currentComponentArray[currentComponentIndex];
+
+  return <main className={animationClass}>{CurrentComponent}</main>;
 }
